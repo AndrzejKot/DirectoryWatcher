5468fa95d25ee3cf600fdc5b8be25494d4fd8935
ankt <andrzej.kotwicki@gft.com>
2017-01-20 15:21:09 +0100

    Websocket handling added.

---
+++ src/main/java/com/gft/message/GreetingMessage.java
@@ -0,0 +1,18 @@
+package com.gft.message;
+
+public class GreetingMessage {
+
+    private String content;
+
+    public GreetingMessage() {
+    }
+
+    public GreetingMessage(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
